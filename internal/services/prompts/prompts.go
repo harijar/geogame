bec6faec7a6f6cb5794f4ac12d69e1c8fc90c577
FKH <redcat1809@gmail.com>
2023-10-28 16:02:39 +0300

    single prompt generation method improved

--- internal/services/prompts/prompts.go
+++ internal/services/prompts/prompts.go
@@ -14,53 +14,57 @@ func New(countriesRepo *repo.CountriesRepository) *Prompts {
 	return &Prompts{countriesRepo: countriesRepo}
 }
 
-func (p *Prompts) GeneratePrompt(country *countries.Country, prevPrompts map[int]string) string {
-	n := rand.Intn(17)
-	for _, ok := prevPrompts[n]; ok; {
-		n = rand.Intn(17)
-	}
+func (p *Prompts) GeneratePrompt(country *countries.Country, prevPrompts []int) string {
 	res := ""
-	switch n {
-	case 0:
-		res = formatCapital(country)
-	case 1:
-		res = formatIndependent(country)
-	case 2:
-		res = formatMonarchy(country)
-	case 3:
-		res = formatReligion(country)
-	case 4:
-		res = formatArea(country)
-	case 5:
-		res = formatPopulation(country)
-	case 6:
-		res = formatGDP(country)
-	case 7:
-		res = formatGDPPerCapita(country)
-	case 8:
-		res = formatHDI(country)
-	case 9:
-		res = formatArgicultural(country)
-	case 10:
-		res = formatService(country)
-	case 11:
-		res = formatUN(country)
-	case 12:
-		res = formatUnrecognised(country)
-	case 13:
-		if len(country.EthnicGroups) != 0 {
-			ethnicGroupID := rand.Intn(len(country.EthnicGroups))
-			res = formatEthnicGroup(country.EthnicGroups[ethnicGroupID])
+	for res == "" {
+		n := rand.Intn(17)
+		for _, id := range prevPrompts {
+			if id == n {
+				continue
+			}
 		}
-	case 14:
-		if len(country.Languages) != 0 {
-			languageID := rand.Intn(len(country.Languages))
-			res = formatLanguage(country.Languages[languageID])
-		}
-	case 15:
-		if len(country.Funfacts) != 0 {
-			funfactID := rand.Intn(len(country.Funfacts))
-			res = formatFunFact(country.Funfacts[funfactID])
+		switch n {
+		case 0:
+			res = formatCapital(country)
+		case 1:
+			res = formatIndependent(country)
+		case 2:
+			res = formatMonarchy(country)
+		case 3:
+			res = formatReligion(country)
+		case 4:
+			res = formatArea(country)
+		case 5:
+			res = formatPopulation(country)
+		case 6:
+			res = formatGDP(country)
+		case 7:
+			res = formatGDPPerCapita(country)
+		case 8:
+			res = formatHDI(country)
+		case 9:
+			res = formatArgicultural(country)
+		case 10:
+			res = formatService(country)
+		case 11:
+			res = formatUN(country)
+		case 12:
+			res = formatUnrecognised(country)
+		case 13:
+			if len(country.EthnicGroups) != 0 {
+				ethnicGroupID := rand.Intn(len(country.EthnicGroups))
+				res = formatEthnicGroup(country.EthnicGroups[ethnicGroupID])
+			}
+		case 14:
+			if len(country.Languages) != 0 {
+				languageID := rand.Intn(len(country.Languages))
+				res = formatLanguage(country.Languages[languageID])
+			}
+		case 15:
+			if len(country.Funfacts) != 0 {
+				funfactID := rand.Intn(len(country.Funfacts))
+				res = formatFunFact(country.Funfacts[funfactID])
+			}
 		}
 	}
 	return res
