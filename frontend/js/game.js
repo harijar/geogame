e7cbe3c090072d125afe85e3a16c7554b0613092
Kirill Nikiforov <allmazz@allmazz.me>
2023-11-11 18:16:41 +0300

    feat: front

--- frontend/js/game.js
+++ frontend/js/game.js
@@ -5,63 +5,72 @@ let prompts = await GetPrompts();
 
 window.onload = async function() {
     await init();
+    let guessButton = document.getElementById("guessButton");
+    let playAgainButton = document.getElementById("playAgainButton");
+    guessButton.onclick = async function() {
+        if (prompts.length > 0) {
+            await guess();
+        } else {
+            window.location.reload();
+        }
+    }
+    playAgainButton.onclick = async function() {
+        window.location.reload();
+    }
 };
 
 async function init() { // should execute on event document loaded
     if (prompts.length < 1) { // if prompts array is empty it means that game hasn't started, otherwise we continue prev game
-        await startGame();
+        prompts = [(await StartGameRequest())['prompt']];
+        SavePrompts(prompts);
     }
     prompts.forEach(function (prompt) {
         showPrompt(prompt);
     })
 }
 
-async function startGame() {
-    prompts = [(await StartGameRequest())['prompt']];
-    SavePrompts(prompts);
-    clearPrompts();
-}
-
 async function guess(){
     let guess = document.getElementById("country").value
     const data = await GuessGameRequest(guess);
     if (data === null) { // game hasn't started
-        await startGame();
+        window.location.reload();
         return;
     } else if (data['right']) { // country guessed
         prompts = [];
+        gameEnded();
         showCountryGuessed(data['country']);
     } else if (data['country']) { // tries limit exceeded
         prompts = [];
+        gameEnded();
         showTriesExceeded(data['country']);
     } else { // wrong guess
         const prompt = data['prompt'];
-        prompts += prompt;
+        prompts.push(prompt);
         showPrompt(prompt);
     }
     await SavePrompts(prompts);
 }
 
 function showPrompt(text) {
-    document.append(`<p style="text-align: center">${text}</p>`)
+    let prompt = document.createElement("p");
+    prompt.style.textAlign = "center";
+    prompt.textContent = text;
+    document.getElementById("promptsDiv").append(prompt);
 }
 
 function showCountryGuessed(country) {
-    document.append(`<p style="text-align: center">You guessed the country! It was ${country}.</p>
-        <button>Play again</button>
-        <button>To the main page</button>`)
+    let notify = document.createElement("p");
+    notify.textContent = `You guessed the country! It was ${country}.`;
+    document.getElementById("promptsDiv").append(notify);
 }
 
 function showTriesExceeded(country) {
-    document.append(`<p style="text-align: center">The country was ${country}.</p>
-        <button>Play again</button>
-        <button>To the main page</button>`)
+    let notify = document.createElement("b");
+    notify.textContent = `The country was ${country}.`;
+    document.getElementById("promptsDiv").append(notify);
 }
 
-function clearPrompts() {
-    const fs = require('fs')
-    fs.readFile('game.html', 'utf-8', (err, data) => {
-        if (err) throw err;
-        document.write(data);
-    })
+function gameEnded() {
+    document.getElementById("guessButton").style.display = 'none';
+    document.getElementById("playAgainButton").style.display = 'inline';
 }
